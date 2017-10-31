facfe49c3f93b71fbc40a1a4eba319c8094d7fad
Tega Brain <tega.brain@gmail.com>
2017-10-31 15:27:34 +1100

    built

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -10,7 +10,9 @@
           What is disobedience in a world shaped by algorithms?
         </h2>
         <p>
-Algorithmic disobedience calls for action in a world shaped by data and structured by algorithms. Algorithmic systems influence what news we see, who we date and how our neighbourhoods are policed. The accumulation of data has become paramount to the ways that power and resources are distributed and yet the logic of these systems are built on patterns drawn out of historic datasets, bringing the conditions of the past back to haunt the present. If individuals are legible only as aggregates of data, as products of pattern and probability, how can we push back? What are strategies that help us ask how we want these systems to be in our lives? This workshop looks to recent work by artists and journalists for tactics of algorithmic disobedience.
+Algorithmic disobedience calls for action in a world shaped by data and structured by algorithms. Algorithmic systems influence what news we see, who we date and how our neighbourhoods are policed. The accumulation of data has become paramount to the ways that power and resources are distributed and yet the logic of these systems are built on patterns drawn out of historic datasets, bringing the conditions of the past back to haunt the present.
+<p></p>
+If individuals are legible only as aggregates of data, as products of pattern and probability, how can we push back? What are strategies that help us ask how we want these systems to be in our lives? This workshop looks to recent work by artists and journalists for tactics of algorithmic disobedience.
 </p>
     </div>
     <div class="section">
